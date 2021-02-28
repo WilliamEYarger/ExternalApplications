2e9138c7e5757aad9ef1eb224c3e0b5e77ec7d94
William Yarger <william.yarger.md@hotmail.com>
2021-02-28 09:27:19 -0500

    Add project files.

---
+++ ExternalApplications/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace ExternalApplications
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+
+        {
+            InitializeComponent();
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+        IntPtr mainWindowHandle = new WindowInteropHelper(window: new Window()).Handle;
+        private void btnOpenExternal_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            if (od.ShowDialog() == true);
+            {
+                Process proc = Process.Start(od.FileName);
+                proc.WaitForInputIdle();
+                while (proc.MainWindowHandle == IntPtr.Zero)
+                {
+                    Thread.Sleep(100);
+                    proc.Refresh();
+                }
+                IntPtr windowHandle = mainWindowHandle;
+            }
+        }
+    }
+}
